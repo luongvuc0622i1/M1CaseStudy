b3fd9f33d282e228070fc4b998f2fc24c5e20c76
Gia Lương <gialuong@MacBook-Pro-cua-Gia.local>
2022-07-29 20:37:21 +0700

    y

---
+++ slider.js
@@ -0,0 +1,4 @@
+const rightbtn = document.getElementById(".fa-chevron-right")
+rightbtn.addEventListener("click",function(){
+    document.querySelector(".slider-content-left-top").style.right = "100%"
+})
