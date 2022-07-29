76ce9c8bbbd86bf41e4e2b2d57969000b2cbce6b
Gia Lương <gialuong@MacBook-Pro-cua-Gia.local>
2022-07-29 22:40:49 +0700

    Hoàn thiện Slider

--- slider.js
+++ slider.js
@@ -1,4 +1,32 @@
-const rightbtn = document.getElementById(".fa-chevron-right")
+const rightbtn = document.querySelector(".fa-chevron-right")
+const leftbtn = document.querySelector(".fa-chevron-left")
+const imgNumber = document.querySelectorAll(".slider-content-left-top img")
+let index = 0
 rightbtn.addEventListener("click",function(){
-    document.querySelector(".slider-content-left-top").style.right = "100%"
+    index = index +1
+    if (index > imgNumber.length-1){
+        index=0
+    }
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
 })
+leftbtn.addEventListener("click",function(){
+    index = index -1
+    if (index <0){
+        index=imgNumber.length-1
+    }
+    document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+})
+//Slider-bottom---------------------------------------------
+const imgNumberLi = document.querySelectorAll(".slider-content-left-bottom li")
+let imgactive = document.querySelector(".active")
+imgNumberLi.forEach(function(image,index){
+    image.addEventListener("click",function () {
+        removeactive ()
+        document.querySelector(".slider-content-left-top").style.right = index *100+"%"
+        image.classList.add("active")
+    })
+})
+function removeactive () {
+    let imgactive = document.querySelector(".active")
+    imgactive.classList.remove("active")
+}
